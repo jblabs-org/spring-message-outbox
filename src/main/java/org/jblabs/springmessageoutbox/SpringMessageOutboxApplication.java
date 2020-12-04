9f7afddfd0d875bb509af2475982e3fa68ae1c76
Joshua Bridevaux <>
2020-12-03 23:01:26 -0600

    initial commit from initializr

---
+++ src/main/java/org/jblabs/springmessageoutbox/SpringMessageOutboxApplication.java
@@ -0,0 +1,13 @@
+package org.jblabs.springmessageoutbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageOutboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessageOutboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jblabs/springmessageoutbox/SpringMessageOutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jblabs.springmessageoutbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageOutboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
