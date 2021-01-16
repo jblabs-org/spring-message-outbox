4ea876d6448f68bfdbf0f6799215e146d9efb34c
Joshua Bridevaux <>
2021-01-16 00:03:30 -0600

    stub module for rabbitmq...sleepy time now

---
+++ outbox-publisher-rabbitmq/src/main/java/org/jblabs/outbox/publisher/rabbitmq/RabbitmqPublisherConfiguration.java
@@ -0,0 +1,9 @@
+package org.jblabs.outbox.publisher.rabbitmq;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RabbitmqPublisherConfiguration {
+}
