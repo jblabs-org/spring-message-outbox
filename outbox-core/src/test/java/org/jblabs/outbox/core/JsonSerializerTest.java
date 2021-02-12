f6bbf724303ec5ddbfd963627bf1e182340242c8
Joshua Bridevaux <>
2021-02-11 21:24:49 -0600

    more test cases

--- outbox-core/src/test/java/org/jblabs/outbox/core/JsonSerializerTest.java
+++ outbox-core/src/test/java/org/jblabs/outbox/core/JsonSerializerTest.java
@@ -1,23 +1,36 @@
 package org.jblabs.outbox.core;
 
+import org.jblabs.outbox.core.message.OutboxMessage;
+import org.jblabs.outbox.core.message.SerializationFailedException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.time.OffsetDateTime;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JsonSerializerTest {
 
     JsonSerializer jsonSerializer = new JsonSerializer();
     String serializedObject = "{\"stringField\":\"test string\",\"intField\":123,\"java8TimestampField\":\"2021-02-11T15:06:02.094443-06:00\"}";
 
+    @Mock
+    OutboxMessage outboxMessage;
+
     @Test
     void serialize() {
         TestObject testObject = testObject();
         String serializedObject = jsonSerializer.serialize(testObject);
-        System.out.println(serializedObject);
         assertThat(serializedObject, is(serializedObject));
+    }
+
+    @Test
+    void serialize_shouldThrowExceptionOnFailure() {
+        TestObject testObject = testObject();
+        assertThrows(SerializationFailedException.class, () -> jsonSerializer.serialize(new WillNotSerialize()));
     }
 
     private TestObject testObject() {
@@ -32,5 +45,11 @@ class JsonSerializerTest {
         public String stringField;
         public Integer intField;
         public OffsetDateTime java8TimestampField;
+    }
+
+    private static class WillNotSerialize {
+        public WillNotSerialize getSelf() {
+            return this;
+        }
     }
 }
---
+++ outbox-core/src/test/java/org/jblabs/outbox/core/message/OutboxMessageTest.java
@@ -0,0 +1,17 @@
+package org.jblabs.outbox.core.message;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class OutboxMessageTest {
+
+    @Test
+    void testConstructor() {
+        OutboxMessage outboxMessage = new OutboxMessage("testName", "testId", "testDest", "testPayload");
+
+        assertThat(outboxMessage.getMessageId(), notNullValue());
+    }
+
+}
