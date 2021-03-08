1de0215cc6898bbcd01a67622adadd38bc50bb84
Joshua Bridevaux <>
2021-03-08 11:56:58 -0600

    adding javadoc

--- outbox-publisher-rabbitmq/src/main/java/org/jblabs/outbox/publisher/rabbitmq/ExchangeNameExtractor.java
+++ outbox-publisher-rabbitmq/src/main/java/org/jblabs/outbox/publisher/rabbitmq/ExchangeNameExtractor.java
@@ -2,6 +2,15 @@ package org.jblabs.outbox.publisher.rabbitmq;
 
 import org.jblabs.outbox.core.message.OutboxMessage;
 
+/**
+ * Interface for classes that determine the exchange name to send an OutboxMessage.
+ */
 public interface ExchangeNameExtractor {
+
+    /**
+     * Determines the exchange to send an {@link OutboxMessage}
+     * @param outboxMessage message to publish
+     * @return name of the exchange to publish the message.
+     */
     String getExchangeName(OutboxMessage outboxMessage);
 }
